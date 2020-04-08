f6a9ce89fc524eb8a16db1962e757be18e7d2b58
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-04-08 17:29:34 +0200

    Implement views. Modify routes. Finish styling.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
+const messages = [
+  { text: 'Call me!', user: 'Jason', added: new Date() },
+  { text: 'You are late.', user: 'Sam', added: new Date() },
+  { text: 'I am hungry', user: 'Walter', added: new Date() }
+]
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New message'});
+})
+
+router.post('/new', function(req, res) {
+  const {name, message} = req.body;
+  messages.push({text: message, user: name, added: new Date()});
+  res.redirect('/');
+})
 
 module.exports = router;
